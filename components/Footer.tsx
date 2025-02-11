55c66bda35f0722f3e0981b9ce9b411afcad17c1
oxytocins <beardslee.stephen@icloud.com>
2025-02-10 20:56:22 -0600

    “THREADSUPDATE2”

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,6 @@
 import Link from "next/link"
 import { Linkedin, Instagram } from "lucide-react"
-
-// Custom Threads Icon
-const Threads = ({ className }: { className?: string }) => (
-  <svg
-    className={className}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-  >
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/>
-  </svg>
-)
+import Image from "next/image" // Import Image component from next/image
 
 export default function Footer() {
   return (
@@ -49,7 +38,7 @@ export default function Footer() {
               rel="noopener noreferrer"
               className="hover:text-pastel-blue transition-colors"
             >
-              <Threads className="w-6 h-6" />
+              <Image src="/icons/threads.svg" alt="Threads" width={24} height={24} />
             </a>
             <a
               href="https://www.instagram.com/oxytocins/"
