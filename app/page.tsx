976b0c2e21e94fdbe68e54a5f1feeeb58b43a692
oxytocins <beardslee.stephen@icloud.com>
2025-02-10 22:00:35 -0600

    SPACING2

--- app/page.tsx
+++ app/page.tsx
@@ -115,7 +115,7 @@ export default function Home() {
 
         {/* Formerly Section */}
         <motion.h2
-          className="text-3xl sm:text-3xl font-medium font-jetbrains-mono max-w-3xl leading-snug mt-2 relative text-left"
+          className="text-3xl sm:text-3xl font-medium font-jetbrains-mono max-w-3xl leading-snug mt-12 relative text-left"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.2 }}
