f7741afafd8d17f17872e943cf4b221316a21206
oxytocins <beardslee.stephen@icloud.com>
2025-02-10 23:10:44 -0600

    FONT

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       fontFamily: {
         "space-grotesk": ["Space Grotesk", "sans-serif"],
         "jetbrains-mono": ["JetBrains Mono", "monospace"],
+        "pragmatica-extended": ["Pragmatica Extended", "sans-serif"], // Add your new font here
       },
       colors: {
         "deep-grey": "#1E1E1E",
