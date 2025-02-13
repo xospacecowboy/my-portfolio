dcf01b8c7df3e89b6772077e8f8b4503f8b90096
oxytocins <beardslee.stephen@icloud.com>
2025-02-13 15:22:46 -0600

    fix: add additional meta tags for favicon and social image

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" type="image/png" href="/favicon.png" />
+        <meta property="og:image" content="https://oxytocins.me/social.png" />
+        <meta name="twitter:image" content="https://oxytocins.me/social.png" />
         <meta name="theme-color" content="#000000" />
       </head>
       <body className={`${spaceGrotesk.className} ${dotGothic16.className}`}>
