ff6156add05c30a041cf4d18b07081fe279d1de2
oxytocins <beardslee.stephen@icloud.com>
2025-02-13 17:00:33 -0600

    fix: simplify Twitter image URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: '@oxytocins - Stephen Beardslee - Creative Strategist & Social Media Leader',
     description: 'Creative strategist and social media leader specializing in digital marketing, brand development, and innovative social campaigns.',
-    images: ['https://opengraph.b-cdn.net/production/images/9ce06c31-916e-44cf-b066-db58997e936d.png?token=grQGwuG57WAHlLm2ZSvROXbW--gLBCujGHPwHFB3_xc&height=630&width=1200&expires=33275484785'],
+    images: 'https://opengraph.b-cdn.net/production/images/9ce06c31-916e-44cf-b066-db58997e936d.png',
     creator: '@oxytocins',
     site: '@oxytocins'
   },
